f1855329d6a156a5dc634fa3be6b3c5efa9c143d
Teten Nugraha <teten.nugraha18@gmail.com>
2022-09-28 05:56:49 +0700

    chore: launch first project
    First initial to build rest api with Kotlin with CRUD and JWT Authentication

---
+++ src/main/kotlin/id/ten/kotlinspringbootjpajwt/KotlinSpringBootJpaJwtApplication.kt
@@ -0,0 +1,11 @@
+package id.ten.kotlinspringbootjpajwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootJpaJwtApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringBootJpaJwtApplication>(*args)
+}
---
+++ src/test/kotlin/id/ten/kotlinspringbootjpajwt/KotlinSpringBootJpaJwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package id.ten.kotlinspringbootjpajwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBootJpaJwtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
